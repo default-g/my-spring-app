9ad65db24e072048ee9a0bf68123cca8c16eb2e4
Artem Obukhov <arteam_101@mail.ru>
2022-12-17 17:43:12 +0700

    Калькулятор через GET запрос

--- src/main/java/com/example/demo/controllers/FirstController.java
+++ src/main/java/com/example/demo/controllers/FirstController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controllers;
 
+import org.mariuszgromada.math.mxparser.Expression;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 @Controller
 public class FirstController {
@@ -13,5 +16,15 @@ public class FirstController {
         model.addAttribute("name", name);
 
         return "greetings";
+    }
+
+    @GetMapping("/first/calculator")
+    public String calculator(@RequestParam(value = "a", required = true) float a,
+                             @RequestParam(value = "b", required = true) float b,
+                             @RequestParam(value = "operand", required = true) String operation,
+                             Model model) {
+        Expression expression = new Expression(a + operation + b);
+        model.addAttribute("result", expression.calculate());
+        return "calculator";
     }
 }
