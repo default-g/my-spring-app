255daa7523db1abbc9f679e9dd1dee0640a2e8ba
Artem Obukhov <arteam_101@mail.ru>
2022-12-21 00:49:27 +0700

    Simple DAO

---
+++ src/main/java/com/example/demo/dao/PersonDAO.java
@@ -0,0 +1,33 @@
+package com.example.demo.dao;
+
+import com.example.demo.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private List<Person> people;
+    private static int PEOPLE_COUNT = 0;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(PEOPLE_COUNT++, "Tom"));
+        people.add(new Person(PEOPLE_COUNT++, "John"));
+        people.add(new Person(PEOPLE_COUNT++, "Fred"));
+        people.add(new Person(PEOPLE_COUNT++, "Mike"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people
+                .stream()
+                .filter(person -> person.getId() == id)
+                .findAny()
+                .orElse(null);
+    }
+}
