3f1e8686e443d167ea702f1997e4c24fff8a3477
Artem Obukhov <arteam_101@mail.ru>
2022-12-24 23:09:42 +0700

    Simple DAO

--- src/main/java/com/example/demo/controllers/PeopleController.java
+++ src/main/java/com/example/demo/controllers/PeopleController.java
@@ -1,21 +1,24 @@
 package com.example.demo.controllers;
 
 import com.example.demo.dao.PersonDAO;
+import com.example.demo.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
 
-    @Autowired
     private PersonDAO personDAO;
 
-    @GetMapping("/")
+    @Autowired
+    private PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
     public String index(Model model) {
         model.addAttribute("people", personDAO.index());
         return "people/index";
@@ -27,6 +30,16 @@ public class PeopleController {
         return "people/show";
     }
 
+    @GetMapping("/new")
+    public String newPerson(Model model) {
+        model.addAttribute("person", new Person());
+        return "people/new";
+    }
 
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
 
 }
--- src/main/java/com/example/demo/dao/PersonDAO.java
+++ src/main/java/com/example/demo/dao/PersonDAO.java
@@ -30,4 +30,10 @@ public class PersonDAO {
                 .findAny()
                 .orElse(null);
     }
+
+    public void save(Person person) {
+        person.setId(PEOPLE_COUNT++);
+        people.add(person);
+    }
+
 }
