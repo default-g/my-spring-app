bd630ab311e35078782f2f139e23b6f62ec5ecfc
Artem Obukhov <arteam_101@mail.ru>
2023-01-29 17:04:33 +0700

    Spring Boot

--- src/main/java/com/example/demo/repositories/BookRepository.java
+++ src/main/java/com/example/demo/repositories/BookRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repositories;
 
 import com.example.demo.models.Book;
 import com.example.demo.models.Person;
+import jakarta.persistence.Column;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
--- src/main/java/com/example/demo/repositories/PeopleRepository.java
+++ src/main/java/com/example/demo/repositories/PeopleRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.repositories;
 
 import com.example.demo.models.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 @Repository
